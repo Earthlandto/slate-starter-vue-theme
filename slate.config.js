6a9f6b28a16b7d3c49802cc2a9cec2be424011df
Alberto López Sánchez <alberto@wolfnoir.com>
2019-01-22 17:11:10 +0100

    Settup Vue configuration

--- slate.config.js
+++ slate.config.js
@@ -3,16 +3,69 @@
 // Configuration file for all things Slate.
 // For more information, visit https://github.com/Shopify/slate/wiki/Slate-Configuration
 
-const path = require('path');
+// const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
+  // An array of string paths to liquid files that associate css variables to liquid variables
   'cssVarLoader.liquidPath': ['src/snippets/css-variables.liquid'],
   'webpack.extend': {
     resolve: {
       alias: {
-        jquery: path.resolve('./node_modules/jquery'),
-        'lodash-es': path.resolve('./node_modules/lodash-es'),
+        vue: 'vue/dist/vue.common.js',
       },
     },
+    module: {
+      rules: [
+        {
+          test: /^[^\.]+\.css$/,
+          use: [
+            {
+              loader: 'style-loader',
+              options: {
+                sourceMap: !isProduction,
+              },
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: !isProduction,
+                importLoaders: 1,
+              },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: !isProduction,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: !isProduction,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+        },
+        {
+          test: /.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    plugins: [new VueLoaderPlugin()],
   },
+  // Object which contains entrypoints used in webpack's config.entry key
+  //'webpack.entrypoints': {},
 };
+
+// useful:
+// https://github.com/Shopify/slate/issues/871
+// https://github.com/liron-navon/slate-vue-starter
