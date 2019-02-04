b18b4c11ba192ff9b33ec3c861c07062c9244830
Alberto López Sánchez <alberto@wolfnoir.com>
2019-02-04 12:38:45 +0100

    Refactorize webpack configuration to a external object

--- slate.config.js
+++ slate.config.js
@@ -18,85 +18,83 @@ const customSlateConfiguration = {
   // (browsersync, asset server) to avoid browsers rejecting requests based
   // on SSL
   'ssl.cert': path.resolve(__dirname, 'certs', 'server.crt'),
+  // An array of string paths to liquid files that associate css variables to liquid variables
+  'cssVarLoader.liquidPath': ['src/snippets/css-variables.liquid'],
+};
+
+const webpackExtendedConfiguration = {
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.common.js',
+      '@vue': path.join(__dirname, 'src/vue'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /^[^\.]+\.css$/,
+        use: [
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isProduction,
+            },
+          },
+
+          {
+            loader: 'style-loader',
+            options: {
+              sourceMap: isProduction,
+            },
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: isProduction,
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: isProduction,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: 'svgs',
+        to: '../snippets/[name].[ext].liquid',
+        toType: 'template',
+      },
+    ]),
+  ],
+  // output: {
+  //   path: path.join(__dirname),
+  // },
 };
 
 module.exports = {
   ...customSlateConfiguration,
-  // An array of string paths to liquid files that associate css variables to liquid variables
-  'cssVarLoader.liquidPath': ['src/snippets/css-variables.liquid'],
   // Extends webpack development config using 'webpack-merge'
   // https://www.npmjs.com/package/webpack-merge
   'webpack.extend': {
-    resolve: {
-      alias: {
-        vue: 'vue/dist/vue.common.js',
-        '@vue': path.join(__dirname, 'src/vue'),
-      },
-    },
-    module: {
-      rules: [
-        {
-          test: /^[^\.]+\.css$/,
-          use: [
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: isProduction,
-              },
-            },
-
-            {
-              loader: 'style-loader',
-              options: {
-                sourceMap: isProduction,
-              },
-            },
-            {
-              loader: 'css-loader',
-              options: {
-                sourceMap: isProduction,
-                importLoaders: 1,
-              },
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-                sourceMap: isProduction,
-              },
-            },
-          ],
-        },
-        {
-          test: /\.vue$/,
-          loader: 'vue-loader',
-        },
-        {
-          test: /.js$/,
-          loader: 'babel-loader',
-          exclude: /node_modules/,
-        },
-      ],
-    },
-    plugins: [
-      new VueLoaderPlugin(),
-      new CopyWebpackPlugin([
-        {
-          from: 'svgs',
-          to: '../snippets/[name].[ext].liquid',
-          toType: 'template',
-        },
-      ]),
-    ],
-
-    // output: {
-    //   path: path.join(__dirname),
-    // },
+    ...webpackExtendedConfiguration,
   },
-  // Enabling sourcemaps in styles when using Hot Module Reloading causes
-  // style-loader to inject styles using a <link> tag instead of <style> tag.
-  // This causes a FOUC content, which can cause issues with JS that is reading
-  // the DOM for styles (width, height, visibility) on page load.
-  'webpack.sourceMap.styles': isProduction,
 
   // Object which contains entrypoints used in webpack's config.entry key
   //'webpack.entrypoints': {},
